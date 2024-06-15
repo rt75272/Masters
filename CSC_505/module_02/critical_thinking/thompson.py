19d715435b10d18a5f008b39de822de41d0f2c70
rt75272 <rt75272@gmail.com>
2024-06-15 09:38:54 -0600

    Init module 2 critical thinking

---
+++ CSC_505/module_02/critical_thinking/thompson.py
@@ -0,0 +1,27 @@
+class thompson:
+    def __init__(self, data):
+        self.data = data
+
+    def communication(self):
+        self.project_initiation = ""
+        self.requirement_gather = ""
+
+    def planning(self):
+        self.estimating = ""
+        self.scheduling = ""
+        self.tracking = ""
+
+    def modeling(self):
+        self.analysis = ""
+        self.design = ""
+
+    def construction(self):
+        self.code = ""
+        self.test = ""
+        self.peer_validation = ""
+    
+    def deployment(self):
+        self.delivery = ""
+        self.support = ""
+        self.feedbaack = ""
+        self.tam = ""
