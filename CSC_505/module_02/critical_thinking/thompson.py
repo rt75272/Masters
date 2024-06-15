d82535e4009b89b0d995e18bb4ab3464105cf8f4
rt75272 <rt75272@gmail.com>
2024-06-15 11:09:37 -0600

    WIP module 2 critical thinking assignment

--- CSC_505/module_02/critical_thinking/thompson.py
+++ CSC_505/module_02/critical_thinking/thompson.py
@@ -3,25 +3,28 @@ class thompson:
         self.data = data
 
     def communication(self):
-        self.project_initiation = ""
-        self.requirement_gather = ""
+        self.project_initiation = "Lets begin"
+        self.requirement_gather = int(input("Enter number of requirements: "))
 
-    def planning(self):
-        self.estimating = ""
-        self.scheduling = ""
-        self.tracking = ""
+    def planning(self, requirements):
+        # Number of requirements multiplied by 2 days(rough estimation). 
+        self.estimating = requirements * 2
+        num_weeks = self.estimating / 5
+        self.scheduling = "The project will be broken up into {num_weeks} weeks" 
+        self.tracking = "Are the requirements being completed on time?"
 
     def modeling(self):
-        self.analysis = ""
-        self.design = ""
+        self.analysis = "Is it a good plan?"
+        self.design = "The design for the requirements?"
+        self.design_review = "Does the design checkout for the requirements?"
 
     def construction(self):
         self.code = ""
-        self.test = ""
-        self.peer_validation = ""
+        self.test = "Running unit test"
+        self.peer_validation = "Have others look over the code."
     
     def deployment(self):
-        self.delivery = ""
-        self.support = ""
-        self.feedbaack = ""
-        self.tam = ""
+        self.delivery = "Deploy the code"
+        self.support = "Maintain the platform"
+        self.feedback = "Gather user feedback"
+        # self.tam = ""
