cc9d2e506da7162feafa5b972ec44a7fdecf852d
rt75272 <rt75272@gmail.com>
2024-06-17 13:26:34 -0600

    WIP shopping list

--- CSC_505/module_03/critical_thinking/shopping.py
+++ CSC_505/module_03/critical_thinking/shopping.py
@@ -4,7 +4,8 @@ while add_more_items:
     item = input("Enter shopping item: ")
     file = open("shopping_list.txt", "a")
     file.write(item + "\n")
-    file.close()
     more_items = input("Add more items?(y/n) ")
     if(more_items == "n"):
         add_more_items = False
+
+file.close()
