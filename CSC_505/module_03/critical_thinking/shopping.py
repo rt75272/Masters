30db9b4130c54e0f72e247ce1dec15eb509b3d18
rt75272 <rt75272@gmail.com>
2024-06-18 15:43:34 -0600

    WIP Shopping list

--- CSC_505/module_03/critical_thinking/shopping.py
+++ CSC_505/module_03/critical_thinking/shopping.py
@@ -1,11 +1,34 @@
-add_more_items = True
+# --------------------------------------------------------------
+# Shopping List
+#
+# Builds ands saves a user specified shopping list
+#
+# Usage:
+#   $ python shopping.py
+# --------------------------------------------------------------
 
-while add_more_items:
-    item = input("Enter shopping item: ")
-    file = open("shopping_list.txt", "a")
-    file.write(item + "\n")
-    more_items = input("Add more items?(y/n) ")
-    if(more_items == "n"):
-        add_more_items = False
+# Main driver function.
+def main():
+    add_more_items = True
 
-file.close()
+    # Continues to loop until no more items are to be added to 
+    # the shopping list.
+    while add_more_items:
+        # Get the shopping list item to be added.
+        item = input("Enter shopping item: ")
+
+        # File to save our shopping list.
+        file = open("shopping_list.txt", "a")
+        file.write(item + "\n")
+
+        # Ask if more items are desired.
+        more_items = input("Add more items?(y/n) ")
+        if(more_items == "n"):
+            add_more_items = False
+    
+    # Close and save our shopping list file.
+    file.close()
+
+# Pushing the big red button.
+if __name__ == "__main__":
+    main()
